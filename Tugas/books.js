32465308e86f7114559ef0dc796df14ec21d44b8
hilalhmdy <mhdhilalhmdy15@gmail.com>
2021-08-28 18:01:19 +0700

    update

---
+++ Tugas/books.js
@@ -0,0 +1,26 @@
+
+
+async function loadJSON(callback) {   
+
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    await xobj.open('GET', 'books.json', true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+ }
+ 
+function init() {
+    loadJSON(function(response) {
+     // Parse JSON string into object
+       var actual_JSON = JSON.parse(response);
+       return actual_JSON;
+    });
+}
+
+var books = init();
+console.log(books);
